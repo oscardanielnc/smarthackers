341e0ca845ac0026157e39d0668d55fe85bd74ad
Oscar Daniel <oscar.navarro@pucp.edu.pe>
2022-01-15 22:26:37 -0500

    first commit

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class NotFound extends React.Component {
+    render() {
+        return (
+            <div>
+                <h3>No se ha encontrado resultados :(</h3>
+            </div>
+        )
+    }
+} 
